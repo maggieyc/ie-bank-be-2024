66bcaa70ee639d3685f6beae7f9982a4f9494bcf
Maggie Chen <97855985+maggieyc@users.noreply.github.com>
2024-10-13 11:51:41 +0200

    add new fields to account model

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -26,7 +26,10 @@ def skull():
 def create_account():
     name = request.json['name']
     currency = request.json['currency']
-    account = Account(name, currency)
+    country = request.json['country']
+    email = request.json['email']
+    type = request.json['type']
+    account = Account(name, currency, country, email, type)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
@@ -63,5 +66,8 @@ def format_account(account):
         'balance': account.balance,
         'currency': account.currency,
         'status': account.status,
-        'created_at': account.created_at
+        'created_at': account.created_at,
+        'email': account.email,
+        'country': account.country,
+        'type': account.type
     }
